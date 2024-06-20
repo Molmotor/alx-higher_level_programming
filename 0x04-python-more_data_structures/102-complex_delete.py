a5293b4fba54a9ddbe1c91da019ee7c806dc14f7
Haitam <haitamessaid19@gmail.com>
2024-06-20 17:53:13 +0100

    damn

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary.keys() ) :
+        if a_dictionary[keys] == value:
+            del a_dictionary[keys]
+    return a_dictionary
+        
