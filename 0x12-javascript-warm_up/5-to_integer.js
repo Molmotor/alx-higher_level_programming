6064969ba4e51224a11c3340200045f2e070d106
Haitam <haitamessaid19@gmail.com>
2024-06-29 11:10:05 -0700

    785

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
-function conv (input) {
-  const num = Number(input);
 
+// Check if there is an argument provided
+if (process.argv.length > 2) {
+  // Get the first argument
+  const arg = process.argv[2];
+
+  // Check if the argument can be converted to an integer
+  const num = parseInt(arg);
+
+  // If the argument is a valid integer, print it
   if (!isNaN(num)) {
-    console.log('My number: ', num);
+    console.log('My number: ' + num);
   } else {
     console.log('Not a number');
   }
+} else {
+  console.log('Not a number');
 }
